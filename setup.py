239e6361ac32a6861ee42b69bbb2ef0a807b1c2d
A. Berçin Barlas <46375571+aysebercin@users.noreply.github.com>
2026-01-21 20:16:07 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="DynaPIN",
-    version="0.1.1",
+    version="0.1.5",
     packages=find_packages(include=["DynaPIN", "DynaPIN.*"]),
     python_requires=">=3.10",
     install_requires=[
